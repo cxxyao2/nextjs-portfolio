6bea938dfc4aee78656f85ad2e1b6c86b447cc1e
jennifer <cxxyao2@gmail.com>
2022-11-23 21:28:44 -0400

    add more links

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -20,25 +20,50 @@ const Skills = () => {
               role='list'
               className='text-center list-none pl-5 space-y-2 text-slate-700'>
               <li>
-                Angular{' '}
-                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+
+
+                <a
+                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                href='https://github.com/cxxyao2/ng-report'>
+           Angular{' '}
+                <RocketLaunchIcon className='inline-block w-6 h-6 text-indigo-600' />
+              </a>
               </li>
               <li>
-                NGRX{' '}
-                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+
+                <a
+                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                href='https://github.com/cxxyao2/ngrx-store'>
+           NGRX{' '}
+                <RocketLaunchIcon className='inline-block w-6 h-6 text-indigo-600' />
+              </a>
               </li>
               <li>
-                React{' '}
-                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+
+                <a
+                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                href='https://github.com/cxxyao2/react-english-notes'>
+           React{' '}
+                <RocketLaunchIcon className='inline-block w-6 h-6 text-indigo-600' />
+              </a>
               </li>
               <li>
-                Redux{' '}
-                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+
+                <a
+                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                href='https://github.com/cxxyao2/redux-start'>
+          Redux{' '}
+                <RocketLaunchIcon className='inline-block w-6 h-6 text-indigo-600' />
+              </a>
               </li>
 
               <li>
-                Next.js{' '}
-                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />{' '}
+                <a
+                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                href='https://github.com/cxxyao2/nextjs-posts'>
+            Next.js{' '}
+                <RocketLaunchIcon className='inline-block w-6 h-6 text-indigo-600' />
+              </a>
               </li>
             </ul>
           </div>
@@ -51,21 +76,37 @@ const Skills = () => {
               role='list'
               className='text-center list-none pl-5 space-y-2 text-slate-700'>
               <li>
-                Mysql{' '}
-                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+                <a
+                  className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                  href='https://github.com/cxxyao2/sketch'>
+                  Mysql
+                  <RocketLaunchIcon className='inline-block w-6 h-6 text-indigo-600' />
+                </a>
               </li>
               <li>
-                MongoDB{' '}
-                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />{' '}
+                <a
+                  className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                  href='https://cxxyao2.github.io/mongoose.html'>
+                  MongoDB
+                  <RocketLaunchIcon className='inline-block w-6 h-6 text-indigo-600' />
+                </a>
               </li>
               <li>
-                Firestore{' '}
-                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />{' '}
+                <a
+                  className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                  href='https://cxxyao2.github.io/angular_crud_and_authentication_firebase.html'>
+                  Firestore
+                  <RocketLaunchIcon className='inline-block w-6 h-6 text-indigo-600' />
+                </a>
               </li>
               <li>Oracle</li>
               <li>
-                GraphQL{' '}
-                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+                <a
+                  className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                  href='https://github.com/cxxyao2/graphql_mongo1'>
+                  GraphQL
+                  <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+                </a>
               </li>
             </ul>
           </div>
@@ -74,36 +115,67 @@ const Skills = () => {
           <h4 className=' text-xl font-semibold text-center pb-2'>Others</h4>
           <ul
             role='list'
-            className='text-center list-none pl-5 space-y-2 text-slate-700'>
+            className='text-center list-none pl-5 space-y-2 text-slate-700 whitespace-pre'>
             <li>TypeScript</li>
             <li>JavaScript</li>
             <li>
               <a
-                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-4'
-                href='https://www.google.com'>
-                Cypress{' '}
+                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                href='https://github.com/cxxyao2/ng-cypress-e2e'>
+                Cypress-Angular
                 <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
               </a>
             </li>
             <li>
-              Github{' '}
-              <RocketLaunchIcon className='inline-block w-6 h-6 text-indigo-600' />
+              <a
+                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                href='https://github.com/cxxyao2/react-test-in-typescript'>
+                Jest-React
+                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+              </a>
             </li>
             <li>
-              Webpack{' '}
-              <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+              <a
+                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                href='https://cxxyao2.github.io/github.html'>
+                Github
+                <RocketLaunchIcon className='inline-block w-6 h-6 text-indigo-600' />
+              </a>
             </li>
             <li>
-              PHP{' '}
-              <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+              <a
+                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                href='https://github.com/cxxyao2/webpack4'>
+                Webpack
+                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+              </a>
             </li>
             <li>
-              Design Pattern{' '}
-              <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+              <a
+                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                href='https://github.com/cxxyao2/sketch'>
+                PHP
+                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+              </a>
             </li>
             <li>
-              Algorithm{' '}
-              <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+
+
+              <a
+                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                href='https://github.com/cxxyao2/DesignPatternDemo'>
+            Design Pattern
+                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+              </a>
+            </li>
+            <li>
+            <a
+                className='cursor-pointer hover:underline hover:decoration-solid underline-offset-8 decoration-indigo-600'
+                href='https://github.com/cxxyao2/algorithm'>
+           Algorithm
+                <CodeBracketIcon className='inline-block w-6 h-6 text-indigo-600' />
+              </a>
+
             </li>
           </ul>
         </div>
